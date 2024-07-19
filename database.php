3342c89c1263a8cb67c5b1c28aea3ab240120d04
Pedro Josué <74951282+pedrosalomaodw@users.noreply.github.com>
2024-07-19 16:38:14 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$bank = new mysqli("localhost", "root", "", "videossql",3306);
+$bank = new mysqli("localhost", "root", "", "namedatabase",port);
 
 if($bank->connect_errno){
     echo "erro ao se conectar", $bank->connect_error;
